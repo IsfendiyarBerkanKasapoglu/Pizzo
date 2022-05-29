c7cbb429bea802c6ff4f8a102795c382e07a17fa
Isfendiyar <berkankasapoglu03@hotmail.com>
2022-05-29 12:49:07 +0300

    change_somethingfromindex

--- configadd.php
+++ configadd.php
@@ -1,16 +1,14 @@
 <?php
+    import_request_variables($_REQUEST);
+    while(!feof)
     $file=fopen("form-save.txt","r");
+
     fread($file,"Name :");
-    fread($file, $username ."\n");
-    fread($file,"Email :");
-    fread($file, $email ."\n");
+    fread($file, $username);
     fread($file,"Password :");
-    fread($file, $password ."\n");
-    fread($file,"Telephone_no :");
-    fread($file, $phone ."\n");
-    fread($file,"Adress :");
-    fread($file, $adres ."\n");
+    fread($file, $password);
     fclose($file);
-    header("location: index.html");
+    header("location: homepage.html");
+    echo $username;
  ?>
 
